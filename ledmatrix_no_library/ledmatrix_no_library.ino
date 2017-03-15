546baeb01c6c48b6542061f3912d7145ac5c81e7
William Madruga <willmadruga@gmail.com>
2017-03-14 21:52:23 -0700

    Sending 'WILL' to the led matrix :)
    1. blink mode
    2. animated mode
    
    TODO: scroll it from right to left, that would be nice.

--- ledmatrix_no_library/ledmatrix_no_library.ino
+++ ledmatrix_no_library/ledmatrix_no_library.ino
@@ -9,14 +9,17 @@
 #define SHIFT_CLK   3
 #define SHIFT_LATCH 4
 
-#define NO_OP 		0x00
-#define DECODE 		0x09
+#define NO_OP     0x00
+#define DECODE    0x09
 #define INTENSITY 0x0A
 #define SCAN_LIM  0x0B
 #define SHUTDOWN  0x0C
 #define TEST_MODE 0x0F
 
-
+// the data
+byte w_[8] = {B11000011,B11000011,B11000011,B11011011,B11011011,B11011011,B11111111,B11111111};
+byte i_[8] = {B00011000,B00011000,B00000000,B00011000,B00011000,B00011000,B00011000,B00011000};
+byte l_[8] = {B01100000,B01100000,B01100000,B01100000,B01100000,B01100000,B01111110,B01111110};
 
 void setup() {
 
@@ -24,40 +27,75 @@ void setup() {
   pinMode(SHIFT_CLK,   OUTPUT);
   pinMode(SHIFT_LATCH, OUTPUT);
 
-	// starts with low intensity - ranges from 0x00 (min) to 0x0F (max)
-	sendData(INTENSITY, 0x00);
+  // starts with low intensity - ranges from 0x00 (min) to 0x0F (max)
+  sendByte(INTENSITY, 0x00);
  
   // setting decode mode to Code B all digits.
   // only the first nibble of data (digit registers D0-D3) will be used.
-  // sendData(DECODE, 0x0F);
+  // sendByte(DECODE, 0x0F);
 
   // scan limit - all pins.
-  sendData(SCAN_LIM, 0x07);
+  sendByte(SCAN_LIM, 0x07);
 
   // max7219 powers up in shutdown mode. This is turning it off, setting up normal mode.
-  sendData(SHUTDOWN, 0x01);
+  sendByte(SHUTDOWN, 0x01);
 
   // blink screen ones in test mode.
-  sendData(TEST_MODE, 0x01);
+  sendByte(TEST_MODE, 0x01);
   delay(500);
-  sendData(TEST_MODE, 0x00);
+  sendByte(TEST_MODE, 0x00);
+
+  clearContent();
 
 }
  
 void loop() {
-
-  // Need to figure out how to show proper data in the LED...
-  for (int i = 1; i <= 8; i++) {
-    sendData(i, 0x01+i);
-    delay(500);
-  }
-
+  blinkLetters();
+  clearContent();
+  animatedLetters();
 }
 
-void sendData(int address, int data) {
+void blinkLetters() {
+  sendLetter(w_);
+  sendLetter(i_);
+  sendLetter(l_);
+  delay(100);
+  sendLetter(l_);
+}
+void animatedLetters() {
+  sendAnimatedLetter(w_);
+  sendAnimatedLetter(i_);
+  sendAnimatedLetter(l_);
+  sendAnimatedLetter(l_);
+}
+
+void sendLetter(byte data[]) {
+  for(int i=0; i<8; i++) {
+    sendByte(i+1, data[i]);
+  }
+  delay(1000);    
+  clearContent();
+}
+
+void sendAnimatedLetter(byte data[]) {  
+  for(int i=0; i<8; i++) {
+    sendByte(i+1, data[i]);
+    delay(200);
+  }
+  delay(500);    
+  clearContent();
+}
+
+void clearContent() {
+  for(int i = 1; i <= 8; i++) {
+    sendByte(i, 0);
+  }
+}
+
+void sendByte(int address, byte data) {
 	
-	// matrix configuration
-	shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, address);
+  // matrix configuration
+  shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, address);
 
   // send data
   shiftOut(SHIFT_DATA, SHIFT_CLK, MSBFIRST, data);
